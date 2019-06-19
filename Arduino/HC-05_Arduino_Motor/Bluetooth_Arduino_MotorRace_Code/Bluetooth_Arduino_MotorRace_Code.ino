606a3b99961dcd9973f59f27f26a8516f65080c1
thole <duy.tho@primelabo.com.vn>
2019-06-19 11:59:55 +0700

    firebase

--- Arduino/HC-05_Arduino_Motor/Bluetooth_Arduino_MotorRace_Code/Bluetooth_Arduino_MotorRace_Code.ino
+++ Arduino/HC-05_Arduino_Motor/Bluetooth_Arduino_MotorRace_Code/Bluetooth_Arduino_MotorRace_Code.ino
@@ -1,12 +1,20 @@
 /*ANDROID ARDUINO BLUETOOTH RC CAR  */ 
 //this is arduino code//
 /*-----------------------code start here -------------------------------------*/
-int led = 3;        //led 
+int led = 8;        //led 
+int led1 = A1;
+int led2 = A2;
+int led3 = A3;
+int Motor_A_Enable = 3;
 int outPin1 = 5;     //motor1 pwm
-int outPin2 = 6;    //motor1 
-int outPin4 = 9;   //motor2 
-int outPin3 = 8;   //motor2 pwm
-char bt = 0;       //BT  
+int outPin2 = 4;    //motor1 
+
+int outPin4 = 7;   //motor2 
+int outPin3 = 6;   //motor2 pwm
+int Motor_B_Enable = 9;
+
+char bt = ' ';       //BT  
+int speedCar = 100;
 /*------------------------------------------------------------------------------*/ 
 void setup() 
 { 
@@ -17,6 +25,9 @@ pinMode(outPin2,OUTPUT);
 pinMode(outPin3,OUTPUT); 
 pinMode(outPin4,OUTPUT); 
 pinMode(led,OUTPUT); 
+pinMode(led1,OUTPUT); 
+pinMode(led2,OUTPUT); 
+pinMode(led3,OUTPUT); 
 } 
 void loop() 
 { 
@@ -25,69 +36,114 @@ if (Serial1.available() > 0)
  bt = Serial1.read(); 
  Serial.println(bt);
  digitalWrite(led, 1); 
+ digitalWrite(led1, 1);
+ digitalWrite(led2, 1);
+ digitalWrite(led3, 1);
  /*________________________________________________________________________*/ 
- if(bt == 'F')        //move forwards 
- { 
-   digitalWrite(outPin1,LOW); 
-   digitalWrite(outPin2,LOW); 
-   digitalWrite(outPin3,HIGH); 
-   digitalWrite(outPin4,LOW); 
- } 
- else if (bt == 'B')       //move backwards 
- { 
-   digitalWrite(outPin1,LOW); 
-   digitalWrite(outPin2,LOW); 
-   digitalWrite(outPin3,LOW); 
-   digitalWrite(outPin4,HIGH); 
- } 
- else if (bt == 'S')     //stop!! 
- {    
-   digitalWrite(outPin1,LOW); 
-   digitalWrite(outPin2,LOW); 
-   digitalWrite(outPin3,LOW); 
-   digitalWrite(outPin4,LOW); 
- } 
- else if (bt == 'R')    //right 
- { 
-   digitalWrite(outPin1,HIGH); 
-   digitalWrite(outPin2,LOW); 
-   digitalWrite(outPin3,LOW); 
-   digitalWrite(outPin4,LOW); 
- } 
- else if (bt == 'L')     //left 
- { 
-   digitalWrite(outPin1,LOW); 
-   digitalWrite(outPin3,LOW); 
-   digitalWrite(outPin2,HIGH); 
-   digitalWrite(outPin4,LOW); 
- } 
- else if (bt == 'I')    //forward right 
- { 
-   
-   digitalWrite(outPin1,HIGH); 
-   digitalWrite(outPin2,LOW); 
-   digitalWrite(outPin3,HIGH); 
-   digitalWrite(outPin4,LOW); 
- } 
- else if (bt == 'G')    //forward left 
- { 
-   digitalWrite(outPin1,LOW); 
-   digitalWrite(outPin2,HIGH); 
-   digitalWrite(outPin3,HIGH);
-   digitalWrite(outPin4,LOW); 
- }
- else if (bt == 'H') {  // back left
-    digitalWrite(outPin1,LOW); 
-   digitalWrite(outPin2,HIGH); 
-   digitalWrite(outPin3,LOW); 
-   digitalWrite(outPin4,HIGH); 
-  }
-  else if (bt == 'J') {  // back right
-    
-    digitalWrite(outPin1,HIGH); 
-   digitalWrite(outPin2,LOW); 
-   digitalWrite(outPin3,LOW); 
-   digitalWrite(outPin4,HIGH); 
+  switch (bt)
+  {
+   case 'F':       //move forwards 
+   { 
+     analogWrite(Motor_A_Enable, speedCar);
+     analogWrite(Motor_B_Enable, speedCar);
+     digitalWrite(outPin1,LOW); 
+     digitalWrite(outPin2,LOW); 
+     digitalWrite(outPin3,HIGH); 
+     digitalWrite(outPin4,LOW); 
+     break;
+   } 
+   case 'B':       //move backwards 
+   { 
+     analogWrite(Motor_A_Enable, speedCar);
+     analogWrite(Motor_B_Enable, speedCar);
+     digitalWrite(outPin1,LOW); 
+     digitalWrite(outPin2,LOW); 
+     digitalWrite(outPin3,LOW); 
+     digitalWrite(outPin4,HIGH); 
+     break;
+   } 
+   case  'S':     //stop!! 
+   {    
+     analogWrite(Motor_A_Enable, speedCar);
+     analogWrite(Motor_B_Enable, speedCar);
+     digitalWrite(outPin1,LOW); 
+     digitalWrite(outPin2,LOW); 
+     digitalWrite(outPin3,LOW); 
+     digitalWrite(outPin4,LOW); 
+     break;
+   } 
+   case 'R':   //right 
+   { 
+     analogWrite(Motor_A_Enable, speedCar);
+     analogWrite(Motor_B_Enable, speedCar);
+     digitalWrite(outPin1,HIGH); 
+     digitalWrite(outPin2,LOW); 
+     digitalWrite(outPin3,LOW); 
+     digitalWrite(outPin4,LOW); 
+     break;
+   } 
+   case 'L':     //left 
+   { 
+     analogWrite(Motor_A_Enable, speedCar);
+     analogWrite(Motor_B_Enable, speedCar);
+     digitalWrite(outPin1,LOW); 
+     digitalWrite(outPin3,LOW); 
+     digitalWrite(outPin2,HIGH); 
+     digitalWrite(outPin4,LOW); 
+     break;
+   } 
+   case 'I':   //forward right 
+   { 
+     analogWrite(Motor_A_Enable, speedCar);
+     analogWrite(Motor_B_Enable, speedCar);
+     digitalWrite(outPin1,HIGH); 
+     digitalWrite(outPin2,LOW); 
+     digitalWrite(outPin3,HIGH); 
+     digitalWrite(outPin4,LOW); 
+     break;
+   } 
+   case 'G':   //forward left 
+   { 
+     analogWrite(Motor_A_Enable, speedCar);
+     analogWrite(Motor_B_Enable, speedCar);
+     digitalWrite(outPin1,LOW); 
+     digitalWrite(outPin2,HIGH); 
+     digitalWrite(outPin3,HIGH);
+     digitalWrite(outPin4,LOW); 
+     break;
+   }
+   case 'H': {  // back left
+     analogWrite(Motor_A_Enable, speedCar);
+     analogWrite(Motor_B_Enable, speedCar);
+     digitalWrite(outPin1,LOW); 
+     digitalWrite(outPin2,HIGH); 
+     digitalWrite(outPin3,LOW); 
+     digitalWrite(outPin4,HIGH); 
+     break;
     }
- } 
+    case 'J': {  // back right
+     analogWrite(Motor_A_Enable, speedCar);
+     analogWrite(Motor_B_Enable, speedCar);
+     digitalWrite(outPin1,HIGH); 
+     digitalWrite(outPin2,LOW); 
+     digitalWrite(outPin3,LOW); 
+     digitalWrite(outPin4,HIGH); 
+     break;
+    }
+    case '1':speedCar = 115;break;
+    case '2':speedCar = 130;break;
+    case '3':speedCar = 145;break;
+    case '4':speedCar = 160;break;
+    case '5':speedCar = 175;break;
+    case '6':speedCar = 190;break;
+    case '7':speedCar = 205;break;
+    case '8':speedCar = 220;break;
+    case '9':speedCar = 235;break;
+    case 'q':speedCar = 255;break;
+    default: //If bluetooth module receives any value not listed above, both motors turn off
+      analogWrite(Motor_A_Enable, 0);
+      analogWrite(Motor_B_Enable, 0);
+        
+   } 
+  }
 } 
