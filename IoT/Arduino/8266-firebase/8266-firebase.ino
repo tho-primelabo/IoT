82178b09d1ec6180bd10c37d703df62f72855edc
thole <duy.tho@primelabo.com.vn>
2019-06-21 13:45:36 +0700

    muti led

--- IoT/Arduino/8266-firebase/8266-firebase.ino
+++ IoT/Arduino/8266-firebase/8266-firebase.ino
@@ -7,19 +7,32 @@
 #define FIREBASE_HOST "winged-bliss-237302.firebaseio.com"
 #define FIREBASE_AUTH "qXwFALB50knO5W4lXOp5jWqIkoNu7lLRufz6HTja"
 
-#define LED 2
+#define LED5 D5
+#define LED1 D1
+#define LED3 D3
+#define LED4 D4
+#define LED6 D6
+#define LED7 D7
+#define LED8 D8
+#define LED9 D9
+#define LED10 D10
 
 #include "DHT.h"
 #define DHTTYPE DHT11 
-#define DHTPIN 4     // what digital pin we're connected to
+#define DHTPIN D2     // D2
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
 
-  pinMode(LED,OUTPUT);
-  
-  digitalWrite(LED,0);
-  
+  pinMode(LED1,OUTPUT);  
+  pinMode(LED3,OUTPUT);  
+  pinMode(LED4,OUTPUT);
+  pinMode(LED5,OUTPUT);
+  pinMode(LED6,OUTPUT);  
+  pinMode(LED7,OUTPUT);
+  pinMode(LED8,OUTPUT);  
+  pinMode(LED9,OUTPUT); 
+   pinMode(LED10,OUTPUT);  
   Serial.begin(9600);
   
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -44,32 +57,98 @@ void setup() {
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   
   Firebase.setInt("LED_STATUS",0);
+  Firebase.setInt("LED_STATUS3",0);
+  Firebase.setInt("LED_STATUS4",0);
+  Firebase.setInt("LED_STATUS5",0);
+  Firebase.setInt("LED_STATUS6",0);
+  Firebase.setInt("LED_STATUS7",0);
+  Firebase.setInt("LED_STATUS8",0);
+  Firebase.setInt("LED_STATUS9",0);
+  Firebase.setInt("LED_STATUS10",0);
 
 }
 
 void loop() {
 
-  if(Firebase.getInt("LED_STATUS") == 1)
-  
-  {
-  
-  digitalWrite(LED,HIGH);
-  
+
+  if(Firebase.getInt("LED_STATUS") == 1)  
+  {  
+    digitalWrite(LED1,HIGH);     
+  }  
+  else  
+  {  
+   digitalWrite(LED1,LOW);   
   }
-  
-  else
-  
-  {
-  
-  digitalWrite(LED,LOW);
-  
+  if(Firebase.getInt("LED_STATUS3") == 1)  
+  {  
+    digitalWrite(LED3,HIGH);     
+  }  
+  else  
+  {  
+   digitalWrite(LED3,LOW);   
+  } if(Firebase.getInt("LED_STATUS4") == 1)  
+  {  
+    digitalWrite(LED4,HIGH);     
+  }  
+  else  
+  {  
+   digitalWrite(LED4,LOW);   
   }
-  
+
+   if(Firebase.getInt("LED_STATUS5") == 1)  
+  {  
+    digitalWrite(LED5,HIGH);     
+  }  
+  else  
+  {  
+   digitalWrite(LED5,LOW);   
+  }
+
+   if(Firebase.getInt("LED_STATUS6") == 1)  
+  {  
+    digitalWrite(LED6,HIGH);     
+  }  
+  else  
+  {  
+   digitalWrite(LED6,LOW);   
+  }
+   if(Firebase.getInt("LED_STATUS7") == 1)  
+  {  
+    digitalWrite(LED7,HIGH);     
+  }  
+  else  
+  {  
+   digitalWrite(LED7,LOW);   
+  }
+   if(Firebase.getInt("LED_STATUS8") == 1)  
+  {  
+    digitalWrite(LED8,HIGH);     
+  }  
+  else  
+  {  
+   digitalWrite(LED8,LOW);   
+  }
+   if(Firebase.getInt("LED_STATUS9") == 1)  
+  {  
+    digitalWrite(LED9,HIGH);  
+  }  
+  else  
+  {  
+   digitalWrite(LED9,LOW);   
+  }
+   if(Firebase.getInt("LED_STATUS10") == 1)  
+  {  
+    digitalWrite(LED10,HIGH);     
+  }  
+  else  
+  {  
+   digitalWrite(LED10,LOW);   
+  }
   if (Firebase.failed()) { // Check for errors 
   
-  Serial.print("setting /number failed:");
+     Serial.print("setting /number failed:");
   
-  Serial.println(Firebase.error());
+     Serial.println(Firebase.error());
   
   return;
   
